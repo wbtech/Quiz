3f9854b98ad98600190803e94a22d0d82eaa2ff6
Osho Wale <90667947+walebee01@users.noreply.github.com>
2022-08-26 11:48:12 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,67 @@
+{
+  const quizData = [
+    {
+        question: 'Country with the largest population in the world?',
+        a: 'Brazil
+        b: 'Nigeria
+        c: 'China
+        d: 'USA
+        correct: 'c',
+    }, 
+    {
+        question: 'what is the most used programming language in 2020?',
+        a: 'Java',
+        b: 'C',
+        c: 'python',
+        d: 'Javascript',
+        correct: 'd',
+    },
+     {
+        question: 'Who is the President of Nigeria?',
+        a: 'Bola Tinubu',
+        b: 'Bukola Saraki',
+        c: 'Muhammadu Buhari',
+        d: 'Dayo Abiodun
+        correct: 'c',
+    },
+        
+     {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+       c: "Help Timothy Move Log"
+        d: 'Helicopters Terminal Motorbots Lamborginis',
+        correct: 'a',
+    },
+    {
+        question: 'what year was Javascript launched?',
+        a: '1997',
+        b: '1995',
+        c: '1994',
+        d: '1996',
+        correct: 'b',
+    },
+     {
+        question: 'which of these is not an African country',
+        a: 'Mozambique',
+        b: 'Peru',
+        c: 'Botswana',
+        d: 'Liberia',
+        correct: 'b',
+    }
+];
+const answerEls = document.querySelectorAll('.answer');
+const quiz = document.getElementById('quiz');
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
