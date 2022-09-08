0ac0ef338c1682e70c8c7d983a4853b119a59a0f
Osho Wale <90667947+walebee01@users.noreply.github.com>
2022-09-08 23:42:10 +0100

    update script.js

--- script.js
+++ script.js
@@ -48,6 +48,54 @@
         c: 'Botswana',
         d: 'Liberia',
         correct: 'b',
+    },
+question: 'Country with the largest population in the world?',
+        a: 'Brazil
+        b: 'Nigeria'
+        c: 'China
+        d: 'USA
+        correct: 'c',
+    }, 
+    {
+        question: 'what is the most used programming language in 2020?',
+        a: 'Java',
+        b: 'C',
+        c: 'python',
+        d: 'Javascript',
+        correct: 'd',
+    },
+     {
+        question: 'Who is the President of Nigeria?',
+        a: 'Bola Tinubu',
+        b: 'Bukola Saraki',
+        c: 'Muhammadu Buhari',
+        d: 'Dayo Abiodun
+        correct: 'c',
+    },
+        
+     {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+       c: "Help Timothy Move Log"
+        d: 'Helicopters Terminal Motorbots Lamborginis',
+        correct: 'a',
+    },
+    {
+        question: 'what year was Javascript launched?',
+        a: '1997',
+        b: '1995',
+        c: '1994',
+        d: '1996',
+        correct: 'b',
+    },
+     {
+        question: 'which of these is not an African country',
+        a: 'Mozambique',
+        b: 'Peru',
+        c: 'Botswana',
+        d: 'Liberia',
+        correct: 'b',
     }
 ];
 
