8b5ffa5e3481d2d36548984fc1288beef00b6395
Wale Osho <90667947+walebee01@users.noreply.github.com>
2022-10-01 13:31:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -94,10 +94,72 @@ question: 'Country with the largest population in the world?',
  const answerEls = document.querySelectorAll('.answer');
 const quiz = document.getElementById('quiz');
 
-quiz.Map(() => {
-  
-})
+const questionE1 =document.getElementById('question');
+const a_text =document.getElementById('a_text');
+const b_text =document.getElementById('b_text');
+const c_text =document.getElementById('c_text');
+const d_text =document.getElementById('d_text');
+const submitbtn = document.getElementById('submit');
+ 
 
+let currentQuiz = 0;
+ let score = 0;
+
+loadQuiz();
+
+function loadQuiz () {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionE1.innerHTML = currentQuizData.question;
+
+    a_text.innerHTML = currentQuizData.a;
+    b_text.innerHTML = currentQuizData.b;
+    c_text.innerHTML = currentQuizData.c;
+    d_text.innerHTML = currentQuizData.d;
+
+}
+
+function getSelected(){
+    
+
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if(answerEl.checked){
+            answer= answerEl.id;
+        }
+    });
+
+        return answer;
+}
+
+function deselectAnswers(){
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitbtn.addEventListener('click', () => {
+    //  check to see the answer
+    const answer = getSelected();
+
+   if(answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++
+        }
+
+       currentQuiz++;
+       if (currentQuiz < quizData.length){
+           loadQuiz();
+       } else {
+       quiz.innerHTML = `<h2>Your Score ${score}/${quizData.lenght}.</h2>`
+        // <button onclick="location.reload()">Reload</button>; to reload the page after quiz
+    };   
+    }
+});
+console.log(quizData.length);
 
   
   
